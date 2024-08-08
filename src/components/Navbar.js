d4df5b30adb39ff4100bdc465e699e63fb10ff45
rohitghatal1 <rohitghatal@gmail.com>
2024-08-08 17:00:38 +0545

    added css for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './navbar.css'
 
 export default function Navbar() {
   return (
     <div>
-      
+      <nav>
+        <div className="navbar">
+            <div className="logo">CV Maker</div>
+            <div className="navitems">
+                <span className="navlinks">Home</span>
+                <span className="navlinks">Services</span>
+                <span className="navlinks">About</span>
+            </div>
+        </div>
+      </nav>
     </div>
   )
 }
