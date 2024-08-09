c0a96f9d1b2940d25c53c896fc98bb038c5d72bf
rohitghatal1 <rohitghatal@gmail.com>
2024-08-09 22:07:03 +0545

    added get started btn and fontawesome icons

--- src/components/HomeTab.js
+++ src/components/HomeTab.js
@@ -8,7 +8,8 @@ export default function HomeTab() {
                 <div className="bannerInfo">
                     <h2>BUILD YOUR PROFESSIONAL CV/RESUME</h2>
                     <h3>IN JUST FEW MINUTS</h3>
-                    <p>Time saving. Easy to Use. Professional</p>
+                    <p><i class="fas fa-clock "></i> Time saving. <i class="fas fa-check-circle"></i> Easy to Use. <i class="fas fa-briefcase"></i> Professional</p>
+                    <button className='getStartedbtn'>Get Started !</button>
                 </div>
             </div>
         </div>
