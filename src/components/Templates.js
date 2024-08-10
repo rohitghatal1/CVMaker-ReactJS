71505f45098567d3affb6267b15ae89c345646a0
rohitghatal1 <rohitghatal@gmail.com>
2024-08-10 12:33:50 +0545

    added template section

---
+++ src/components/Templates.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Templates() {
+  return (
+    <div>
+      <section className="templatesSection">
+        <h2>Here are the <span>Best Template Designs</span> for you</h2>
+        <p>An outstanding resume opens the door; a great interview seals the deal.</p>
+        <div className="templateContainer">
+
+        </div>
+      </section>
+    </div>
+  )
+}
