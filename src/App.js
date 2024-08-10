812f11e932092e1964dbf1efb4055b1872c87cd3
rohitghatal1 <rohitghatal@gmail.com>
2024-08-10 12:44:37 +0545

    added css for templates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import HomeTab from './components/HomeTab';
 import Navbar from './components/Navbar';
+import Templates from './components/Templates';
 
 function App() {
   return (
     <>
       <Navbar/>
       <HomeTab/>
+      <Templates/>
     </>
   );
 }
--- src/components/Templates.js
+++ src/components/Templates.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './templates.css'
 
 export default function Templates() {
   return (
     <div>
       <section className="templatesSection">
-        <h2>Here are the <span>Best Template Designs</span> for you</h2>
-        <p>An outstanding resume opens the door; a great interview seals the deal.</p>
+        <h2>Here are the Best <span>Template Designs</span> for you</h2>
+        <p>An outstanding resume opens the door. A great interview seals the deal</p>
         <div className="templateContainer">
 
         </div>
