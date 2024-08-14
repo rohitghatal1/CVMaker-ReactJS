b8f4057c3a10e6559d6ac33a879158c48e73990c
rohitghatal1 <rohitghatal@gmail.com>
2024-08-14 13:07:21 +0545

    added dynamically changing and selecting template

--- src/components/Templates.js
+++ src/components/Templates.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import templateData from '../templateSelector/templateData'
 import './templates.css'
 
 export default function Templates() {
+  const [selectedTemplate, setSelectedTemplate] = useState(null);
   return (
     <div>
       <section className="templatesSection">
         <h2>Here are the Best <span>Template Designs</span> for you</h2>
         <p>An outstanding resume opens the door. A great interview seals the deal</p>
         <div className="templateContainer">
-
+          <Swiper
+            spaceBetween={30}
+            slidesPerView = {3}
+            loop = {true}
+            pagination = {{clickable: true}}
+            navigation = {true}
+          >
+          {templateData.map((template) =>(
+            <SwiperSlide key={template.id}>
+              <div
+                className="templateCard"
+                onClick={()=> setSelectedTemplate(template.component)}
+              >
+                <img src={template.image} alt={template.name} />
+                <h3>{template.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+          </Swiper>
         </div>
+        {selectedTemplate && (
+          <div className="templatePreview">
+            <h2>Preview of Selected Template</h2>
+            <selectedTemplate data = {lkjlk}/>
+          </div>
+        )}
       </section>
     </div>
   )
---
+++ src/templateSelector/templateData.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import professionalTemplate from "../allTemplates/professionalTemplate/professionalTemplate";
+
+const templateData = [
+    {
+        id: 1,
+        name: 'Professional Template',
+        image: 'assets/images/professionalTemplage.png',
+        component: professionalTemplate
+    }
+];
+
+export default templateData;
