63580a47071cd8b6b87f733468388c580be09d0d
rohitghatal1 <rohitghatal@gmail.com>
2024-08-18 13:00:30 +0545

    added basic template

---
+++ src/allTemplates/basicTemplate/BasicTemplate.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function BasicTemplate() {
+    return (
+        <div>
+            <div class="container">
+                <div class="left-section">
+                    <div class="profile-picture">
+                        <img src="profile.jpg" alt="Profile Picture" />
+                    </div>
+                    <h1>SUJAN KAYSTHA</h1>
+                    <h2>INTERFACE DESIGNER</h2>
+
+                    <div class="section summary">
+                        <h3>SUMMARY</h3>
+                        <ul>
+                            <li><i class="fas fa-phone"></i> +977 1 9856 444 888</li>
+                            <li><i class="fas fa-envelope"></i> youremail@domain.com</li>
+                            <li><i class="fas fa-map-marker-alt"></i> Dillibazar 13, Kathmandu</li>
+                            <li><i class="fab fa-facebook"></i> Facebook.com/archana.56</li>
+                        </ul>
+                    </div>
+
+                    <div class="section awards">
+                        <h3>AWARDS</h3>
+                        <p><strong>Best Travel App</strong><br />Vue Intacts / 2020 / Kathmandu<br />Improve information architecture of ABC's Web site by assisting with</p>
+                        <p><strong>UI Design 2018</strong><br />Sunsex Global / 2018 / Nairobi<br />Overall responsibilities included managing a portfolio of customers</p>
+                    </div>
+
+                    <div class="section achievements">
+                        <h3>ACHIEVEMENTS</h3>
+                        <p><strong>Designed Nepal Wallet App</strong><br />Partnered with user interface designers, Web developers and UX</p>
+                        <p><strong>User Research Web Presence</strong><br />Completed user research to identify opportunities for improving XYZ's</p>
+                        <p><strong>Success in Cross Platform Digital Money Connection</strong><br />Partnered with user interface designers, Web developers and UX</p>
+                    </div>
+                </div>
+
+                <div class="right-section">
+                    <div class="section summary">
+                        <h3>SUMMARY</h3>
+                        <p>Consistently ranked in the top ten country managers for UNDP as measured by overall revenues generated and year over year increase in revenues...</p>
+                    </div>
+
+                    <div class="section education">
+                        <h3>EDUCATION</h3>
+                        <p><strong>Master in Interface Design</strong><br />Pulchok Engineering College / Pulchowk / Mar, 2018 - Apr 2021<br />Overall responsibilities included managing a portfolio of customers with total value of $35 million in revenue...</p>
+                        <p><strong>Bachelor in Computer Application</strong><br />CSE College / Dhulikhel / Mar, 2015 - Apr 2018<br />Completed user research to identify opportunities for improving XYZ's and ABC's Web presence...</p>
+                    </div>
+
+                    <div class="section work-experience">
+                        <h3>WORK EXPERIENCE</h3>
+                        <p><strong>Product Manager</strong><br />Brain Workshop / Pulchowk / Mar, 2018 - Apr 2021<br />Overall responsibilities included managing a portfolio of customers with total value of $35 million in revenue...</p>
+                        <p><strong>UI Designer</strong><br />Karkhana Inc / Dhulikhel / Mar, 2015 - Apr 2018<br />Completed user research to identify opportunities for improving XYZ's and ABC's Web presence...</p>
+                        <p><strong>Graphics Designer</strong><br />Thompson Advertising / Jawalakhel / Mar, 2014 - Apr 2015<br />Completed user research to identify opportunities for improving XYZ's and ABC's Web presence...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
