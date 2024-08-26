fb18e7dab1538148964a3a19d7a6dea029aa027b
rohitghatal1 <rohitghatal@gmail.com>
2024-08-26 13:17:29 +0545

    added menu items

--- src/components/CVgenerator.js
+++ src/components/CVgenerator.js
@@ -10,6 +10,36 @@ export default function CVgenerator() {
                             <i class="fa-regular fa-copy"></i>
                             <h3>Templates</h3>
                         </div>
+
+                        <div className="stepItem">
+                            <i class="fa-regular fa-user"></i>
+                            <h3>Personal</h3>
+                        </div>
+
+                        <div className="stepItem">
+                            <i class="fa-solid fa-graduation-cap"></i>
+                            <h3>Education</h3>
+                        </div>
+
+                        <div className="stepItem">
+                            <i class="fa-solid fa-layer-group"></i>
+                            <h3>Experience</h3>
+                        </div>
+
+                        <div className="stepItem">
+                            <i class="fa-solid fa-rocket"></i>
+                            <h3>Projects</h3>
+                        </div>
+
+                        <div className="stepItem">
+                            <i className="fa-solid fa-code-compare"></i>
+                            <h3>Skills</h3>
+                        </div>
+
+                        <div className="stepItem">
+                            <i class="fa-solid fa-star"></i>
+                            <h3>Achievement</h3>
+                        </div>
                     </div>
                 </aside>
             </div>
