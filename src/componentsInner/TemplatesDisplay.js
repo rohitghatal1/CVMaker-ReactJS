eed0c962b78b99ac17138310f6beb7f646dfd296
rohitghatal1 <rohitghatal@gmail.com>
2024-08-27 13:13:58 +0545

    added template display info

--- src/componentsInner/TemplatesDisplay.js
+++ src/componentsInner/TemplatesDisplay.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './templatesDisplay.css'
 
 export default function TemplatesDisplay() {
   return (
     <div>
-      
+      <div className="allTemplateDisplay">
+        <h2>Select a Template</h2>
+        div.templatesContainer
+      </div>
     </div>
   )
 }
