a6187753e0c08aa1e2ac179fc30901b271364f1f
rohitghatal1 <rohitghatal@gmail.com>
2024-08-28 16:06:47 +0545

    added css and upload photo section

--- src/componentsInner/PersonalData.js
+++ src/componentsInner/PersonalData.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import './personal.css'
 
 export default function PersonalData() {
   return (
     <div>
       <div className="personalInfoCollector">
-        <h5>here teh personal data is collected</h5>
+        <h2>About Yourself</h2>
+        <p>Fill your personal information</p>
+
+        <div className="personalInformation">
+            <div className="photoSection">
+                <input type="file" />
+                <label htmlFor="photo">Upload Photo</label>
+
+            </div>
+        </div>
       </div>
     </div>
   )
