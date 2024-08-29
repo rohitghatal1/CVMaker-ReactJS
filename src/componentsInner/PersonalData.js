fdf79edcacc0a519ff8ed877fc67f1903d045231
rohitghatal1 <rohitghatal@gmail.com>
2024-08-29 14:34:37 +0545

    added upload and delete options

--- src/componentsInner/PersonalData.js
+++ src/componentsInner/PersonalData.js
@@ -10,12 +10,16 @@ export default function PersonalData() {
         <p>Fill your personal information</p>
 
         <div className="personalInformation">
-            <div className="photoSection">
-                <figure className='personPhoto'>
-                    <img src={personPhoto} alt="photo" />
-                </figure>
+          <div className="photoSection">
+            <figure className='personPhoto'>
+              <img src={personPhoto} alt="photo" />
+            </figure>
 
+            <div className="uploadDeleteBtn">
+              <span className='uploadbtn'><i class="fa-solid fa-pencil"></i> Upload Photo</span>
+              <span className='deletebtn'><i class="fa-regular fa-trash-can"></i> Delete Photo</span>
             </div>
+          </div>
         </div>
       </div>
     </div>
