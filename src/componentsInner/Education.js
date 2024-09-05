636f37c1c8356fa6f0d6d48cb3cd87a5cd63c065
rohitghatal1 <rohitghatal@gmail.com>
2024-09-05 13:57:44 +0545

    added modal for add new educaton details

--- src/componentsInner/Education.js
+++ src/componentsInner/Education.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './education.css'
 
 export default function Education() {
+  const [education, setEduacation] = useState([]);
+  const [newEducation, setNewEducation] = useState();
+  const [isModalOpen, setisModalOpen] = useState(false);
+
+  const hanldeAddEducation = ()=> {
+    setisModalOpen(true);
+  }
   return (
     <div>
       <div className="educationComponent">
@@ -14,7 +21,25 @@ export default function Education() {
             </div>
             <div className="addNewEducationDetail">
                 <p>New Details</p>
-                <button className='addNewEducationbtn'><i className='fas fa-plus'></i> Add Education</button>
+                {isModalOpen && (
+                  <div className="addEducationModal">
+                    <h2>Education Details</h2>
+                    <form className="educationForm">
+                      <label htmlFor="Degree">Degree:</label>
+                      <input type="text" placeholder='eg. Bachelors, masters etc.' />
+
+                      <label htmlFor="clz">College Name:</label>
+                      <input type="text" placeholder='Enter name of college' />
+
+                      <label htmlFor="started">Started Date</label>
+                      <input type="date" />
+                      
+                      <label htmlFor="passedYear">Passed Date</label>
+                      <input type="date" />
+                    </form>
+                  </div>
+                )}
+                <button className='addNewEducationbtn' onClick={hanldeAddEducation}><i className='fas fa-plus'></i> Add Education</button>
             </div>
         </section>
       </div>
