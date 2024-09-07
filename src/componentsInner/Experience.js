8df4019e9ee0368e84b82d8a8d6c3601b9b3b5d2
rohitghatal1 <rohitghatal@gmail.com>
2024-09-07 08:55:52 +0545

    added next and back buttons

--- src/componentsInner/Experience.js
+++ src/componentsInner/Experience.js
@@ -2,23 +2,28 @@ import React from 'react'
 import './experience.css'
 
 export default function Experience() {
-  return (
-    <div>
-      <div className="experienceComponent">
-        <h2>Experience</h2>
-        <p>Provide your Experience details here</p>
+    return (
+        <div>
+            <div className="experienceComponent">
+                <h2>Experience</h2>
+                <p>Provide your Experience details here</p>
 
-        <section className="experienceSection">
-            <div className="currentAddedExperience">
-                <p>Experience</p>
-            </div>
+                <section className="experienceSection">
+                    <div className="currentAddedExperience">
+                        <p>Experience</p>
+                    </div>
 
-            <div className="addNewExperiecne">
-                <p>New details</p>
-                <button className='addNewExperiencebtn'><i className='fas fa-plus'></i> Add Experience</button>
+                    <div className="addNewExperiecne">
+                        <p>New details</p>
+                        <button className='addNewExperiencebtn'><i className='fas fa-plus'></i> Add Experience</button>
+                    </div>
+
+                    <div className="nextAndBackBtn">
+                        <button className='prevBtn'><i class="fa-solid fa-arrow-left"></i> Previous</button>
+                        <button className='nextBtn'>Next <i class="fa-solid fa-arrow-right"></i></button>
+                    </div>
+                </section>
             </div>
-        </section>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
