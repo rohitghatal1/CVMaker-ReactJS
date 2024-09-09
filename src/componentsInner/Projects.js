42088fd754ccea693a8f9d9b12ba008f30fb6400
rohitghatal1 <rohitghatal@gmail.com>
2024-09-09 15:32:44 +0545

    added projects sectio and style

--- src/componentsInner/Projects.js
+++ src/componentsInner/Projects.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './projects.css'
 
 export default function Projects() {
   return (
     <div>
       <div className="projectsComponent">
-        <h2>Thsi is project part!!</h2>
+        <h2>Projects</h2>
+        <p>List Out your Projects</p>
+
+        <section className="projectsSection">
+            <div className="addedProjects">
+                <p>Current Projects</p>
+            </div>
+
+            <div className="newProjects">
+
+            </div>
+        </section>
       </div>
     </div>
   )
