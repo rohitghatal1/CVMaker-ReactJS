cee893a12266fc6e0cd1a1d1ad090d4affee3fb0
rohitghatal1 <rohitghatal@gmail.com>
2024-09-10 14:48:25 +0545

    added add new projct modal

--- src/componentsInner/Projects.js
+++ src/componentsInner/Projects.js
@@ -2,9 +2,13 @@ import React from 'react'
 import './projects.css'
 
 export default function Projects() {
-    const openAddProjectModal = () => {
-        
-    }
+  const openAddProjectModal = () => {
+
+  }
+
+  const handleCloseForm = () =>{
+    
+  }
   return (
     <div>
       <div className="projectsComponent">
@@ -12,15 +16,30 @@ export default function Projects() {
         <p>List Out your Projects</p>
 
         <section className="projectsSection">
-            <div className="addedProjects">
-                <p>Current Projects</p>
+          <div className="addedProjects">
+            <p>Current Projects</p>
+          </div>
+
+          <div className="newProjects">
+            <p>New Projects</p>
+
+            <div className="addNewProjectModal">
+              <form className='projectForm'>
+                <label htmlFor="pTitle">Project Title</label>
+                <input type="text" name="projectTitle" placeholder='Title of your project' />
+
+                <label htmlFor="pDescription">Description</label>
+                <input type="text" name='projectDesc' placeholder='tell what your project is about' />
+
+                <div className="submitAndCloseBtns">
+                  <button type='submit' className='submitBtn'>Submit</button>
+                  <button className='closeBtn' onClick={handleCloseForm}>Close</button>
+                </div>
+              </form>
             </div>
 
-            <div className="newProjects">
-                <p>New Projects</p>
-
-                <button className='addNewProjectbtn' onClick={openAddProjectModal}><i className='fas fa-plus'></i> Add Experience</button>
-            </div>
+            <button className='addNewProjectbtn' onClick={openAddProjectModal}><i className='fas fa-plus'></i> Add Experience</button>
+          </div>
         </section>
       </div>
     </div>
