c5c5b2dfa4dc26d5e5af154db1629bf4d06868b7
rohitghatal1 <rohitghatal@gmail.com>
2024-09-11 14:53:23 +0545

    added add skill modal

--- src/componentsInner/Skills.js
+++ src/componentsInner/Skills.js
@@ -3,7 +3,7 @@ import './skills.css'
 
 export default function Skills() {
 
-  const openAddSkillModal = () =>{
+  const openAddSkillModal = () => {
 
   }
   return (
@@ -19,6 +19,21 @@ export default function Skills() {
           <div className="newSkills">
             <p>New Skills</p>
 
+            <div className="addSkillModal">
+              <form className='addSkillForm'>
+                <div className="row">
+                
+                  <input type="text" placeholder='eg. HTML, CSS, JAVA, C ...' />
+                  
+                  <select name="skillLevel">
+                    <option value="Beginner">Beginner</option>
+                    <option value="Intermediate">Intermediate</option>
+                    <option value="Experienced">Experienced</option>
+                    <option value="Expert">Expert</option>
+                  </select>
+                </div>
+              </form>
+            </div>
             <button className='addNewSkillbtn' onClick={openAddSkillModal}><i className='fas fa-plus'></i> Add Skill</button>
           </div>
         </section>
