094fc969e21e551aa60edd5ded9d129f7a8baf7e
rohitghatal1 <rohitghatal@gmail.com>
2024-09-11 14:36:03 +0545

    added skills section and css

--- src/componentsInner/Skills.js
+++ src/componentsInner/Skills.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './skills.css'
 
 export default function Skills() {
   return (
     <div>
       <div className="skillsComponent">
-        <h2>This is skills component !!</h2>
+        <h2>Skills</h2>
+        <p>Provide Your Skill Details</p>
+        <section className="skillSection">
+          <div className="addedSkills">
+            <p>Skills</p>
+          </div>
+        </section>
       </div>
     </div>
   )
