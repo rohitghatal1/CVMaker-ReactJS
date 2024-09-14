f6712340c7ed4679c51f9d7a7b54c2224f9ea44c
rohitghatal1 <rohitghatal@gmail.com>
2024-09-14 20:37:34 +0545

    added about us component

---
+++ src/components/aboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function aboutUs() {
+  return (
+    <div>
+      <div className="aboutUsConponent">
+        <h2>About Us</h2>
+      </div>
+    </div>
+  )
+}
