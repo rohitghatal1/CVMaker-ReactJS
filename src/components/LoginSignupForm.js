f032999704e8574f66c609f109869bb24f597acd
rohitghatal1 <rohitghatal@gmail.com>
2024-09-15 16:14:21 +0545

    added login form

--- src/components/LoginSignupForm.js
+++ src/components/LoginSignupForm.js
@@ -4,7 +4,18 @@ export default function LoginSignupForm() {
   return (
     <div>
       <div className="loginSignupModal">
-        <div className="loginModal"></div>
+        <div className="loginModal">
+            <h2>User Login</h2>
+            <form action="" className="loginform">
+                <label htmlFor="username">Username</label>
+                <input type="text" />
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="" />
+
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account? <span>Create here...</span></p>
+        </div>
       </div>
     </div>
   )
