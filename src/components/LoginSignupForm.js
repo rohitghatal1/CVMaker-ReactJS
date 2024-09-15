a2af8c3cdd15eef5e71a281a7d1d4c8303a03f61
rohitghatal1 <rohitghatal@gmail.com>
2024-09-15 16:18:16 +0545

    added signup form

--- src/components/LoginSignupForm.js
+++ src/components/LoginSignupForm.js
@@ -1,22 +1,45 @@
 import React from 'react'
 
 export default function LoginSignupForm() {
-  return (
-    <div>
-      <div className="loginSignupModal">
-        <div className="loginModal">
-            <h2>User Login</h2>
-            <form action="" className="loginform">
-                <label htmlFor="username">Username</label>
-                <input type="text" />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="" />
+    return (
+        <div>
+            <div className="loginSignupModal">
+                <div className="loginModal">
+                    <h2>User Login</h2>
+                    <form action="" className="loginform">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
 
-                <button type="submit">Login</button>
-            </form>
-            <p>Don't have an account? <span>Create here...</span></p>
+                        <button type="submit">Login</button>
+                    </form>
+                    <p>Don't have an account? <span>Create here...</span></p>
+                </div>
+                <div className="signupModal">
+                    <h2></h2>
+                    <form className="signupForm">
+                        <label htmlFor="fname">Full Name</label>
+                        <input type="text" />
+
+                        <label htmlFor="address">Address</label>
+                        <input type="text" />
+
+                        <label htmlFor="age">Age</label>
+                        <input type="text" />
+
+                        <label htmlFor="username">Username</label>
+                        <input type="text" />
+
+                        <label htmlFor="pass">Password</label>
+                        <input type="password" />
+
+                        <label htmlFor="cpass">Confirm Password</label>
+                        <input type="password" />
+                    </form>
+                    <p>Already have an account?<span>Login here</span></p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
