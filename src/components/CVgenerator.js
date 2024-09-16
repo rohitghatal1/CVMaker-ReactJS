309c11125a34b959e87742f3302d3f51e595f119
rohitghatal1 <rohitghatal@gmail.com>
2024-09-16 16:28:14 +0545

    added function to retreive dat from local storage when genereate button is clicked

--- src/components/CVgenerator.js
+++ src/components/CVgenerator.js
@@ -7,11 +7,13 @@ import Experience from '../componentsInner/Experience';
 import Projects from '../componentsInner/Projects';
 import Skills from '../componentsInner/Skills';
 import Achievements from '../componentsInner/Achievements'
+import FinalCV from './FinalCV';
 
 export default function CVgenerator() {
     const steps = ['Templates', 'Personal', 'Education', 'Experience', 'Projects', 'Skills', 'Achievement'];
     const [activeStepIndex, setActiveStepIndex] = useState(0);
     const [selectedTemplate, setSelectedTemplate] = useState(null);
+    const [finalCV, setFinalCV] = useState(null);
 
     const goToNextStep = () => {
         if(activeStepIndex < steps.length - 1){
@@ -24,6 +26,25 @@ export default function CVgenerator() {
             setActiveStepIndex(activeStepIndex - 1)
         }
     }
+
+    const generateCV = () => {
+        const personalData = JSON.parse(localStorage.getItem('personalData'));
+        const educationData = JSON.parse(localStorage.getItem('educationData'));
+        const experienceData = JSON.parse(localStorage.getItem('experienceData'));
+        const projectsData = JSON.parse(localStorage.getItem('projectsData'));
+        const skillsData = JSON.parse(localStorage.getItem('skillsData'));
+        const achievementData = JSON.parse(localStorage.getItem('achievementData'));
+
+        setFinalCV({
+            template: selectedTemplate,
+            personalData,
+            educationData,
+            experienceData,
+            projectsData,
+            skillsData,
+            achievementData
+        })
+    };
 
     const currentStep = steps[activeStepIndex];
 
@@ -48,7 +69,18 @@ export default function CVgenerator() {
                         {currentStep === 'Experience' && <Experience goToPreviousStep = {goToPreviousStep} goToNextStep = {goToNextStep} />}
                         {currentStep === 'Projects' && <Projects goToPreviousStep = {goToPreviousStep} goToNextStep = {goToNextStep} />}
                         {currentStep === 'Skills' && <Skills goToPreviousStep = {goToPreviousStep} goToNextStep = {goToNextStep} />}
-                        {currentStep === 'Achievement' && <Achievements goToPreviousStep = {goToPreviousStep} goToNextStep = {goToNextStep} />}
+                        {currentStep === 'Achievement' && <Achievements goToPreviousStep = {goToPreviousStep} generateCV = {generateCV} />}
+                        {finalCV &&(
+                            <FinalCV
+                                template = {finalCV.template}
+                                personalData = {finalCV.personalData}
+                                educationData = {finalCV.educationData}
+                                experienceData = {finalCV.experienceData}
+                                projectsData = {finalCV.projectsData}
+                                skillsData = {finalCV.skillsData}
+                                achievementData = {finalCV.achievementData} 
+                            />
+                        )}
                     </div>
             </div>
         </div>
---
+++ src/components/FinalCV.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FinalCV() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
