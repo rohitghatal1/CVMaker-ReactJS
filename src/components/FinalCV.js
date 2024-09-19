6747386bc7ddcbb5d04e817163cfe62fb8dfc284
rohitghatal1 <rohitghatal@gmail.com>
2024-09-19 17:54:57 +0545

    added direct send of component

--- src/components/FinalCV.js
+++ src/components/FinalCV.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-export default function FinalCV({ template: Template }) {
+export default function FinalCV(template) {
   return (
     <div>
       <div className="finalCVContainer">
-        <h2>Here is your final CV</h2>
+        <h2>Here is you final CV</h2>
+
         <div className="finalCVSection">
-          {Template ? <Template /> : <p>No template selected</p>}
+            {React.createElement(template)}
         </div>
       </div>
     </div>
-  );
+  )
 }
