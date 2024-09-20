8befff532474111dc7666117ef619fcacd101953
rohitghatal1 <rohitghatal@gmail.com>
2024-09-20 16:06:30 +0545

    added useRef for downloading cv

--- src/components/FinalCV.js
+++ src/components/FinalCV.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './finalcv.css'
+import html2pdf from 'html2pdf.js';
 
 export default function FinalCV({ template }) {
+  const cvRef = useRef();
+
+  const downloadCV = () => {
+    const element = cvRef.current;
+    const options = {
+      margin: 1,
+      filename: 'cv.pdf',
+      imag: {type: 'jpeg', quality: 0.98},
+      html2canvas: {scale: 2},
+      jsPDF: {unit: 'in', format: 'letter', orientation: 'portrait'}
+    };
+    html2pdf().from(element).set(options).save();
+  };
+
   return (
     <div>
-      <div className="finalCVContainer">
+      <div className="finalCVSection">
+      <div className="headingAndDownloadBtn">
         <h2 className='cvHeading'>Here is your final CV</h2>
+        <button onClick={downloadCV} className='downloadCVBtn'>Download CV</button>
+      </div>
 
-        <div className="finalCVSection">
+        <div className="finalCVContainer" ref={cvRef}>
           {template 
 
             ? React.cloneElement(template) // Render the selected template component directly
