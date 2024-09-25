70f96622159f24e3ec17e0f37715a241d61554c8
rohitghatal1 <rohitghatal@gmail.com>
2024-09-25 21:58:49 +0545

    added executive template

---
+++ src/allTemplates/executiveTemplate/ExecutiveTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ExecutiveTemplate() {
+  return (
+    <div>
+      <div className="executiveTemplate">
+        <h2>This is a executive template</h2>
+      </div>
+    </div>
+  )
+}
