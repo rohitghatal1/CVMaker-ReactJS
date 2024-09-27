10f6406d33c19aece7c0c89d687930605af107be
rohitghatal1 <rohitghatal@gmail.com>
2024-09-27 17:32:32 +0545

    added templates tab

---
+++ src/components/TemplateTab.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function TemplateTab() {
+  return (
+    <div>
+      <div className="TemplateTab">
+        <h2>Template</h2>
+        <p>Here Are the list of <span>Templates</span> available!!!</p>
+
+        <div className="templates-Container">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
