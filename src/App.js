4e5a921b8beb11bb055df7f57e34f0d01b6f1cd9
rohitghatal1 <rohitghatal@gmail.com>
2024-09-28 16:19:21 +0545

    added top loader using nprogress

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import BasicTemplate from './allTemplates/basicTemplate/BasicTemplate';
 import CreativeTemplate from './allTemplates/CreativeTemplate/CreativeTemplate';
 import ProfessionalIITemplate from './allTemplates/ProfessionalIITemplate/ProfessionalIITemplate';
 import ProfessionalTemplate from './allTemplates/professionalTemplate/ProfessionalTemplate';
-import './App.css';
+import './App.css'; // Import your custom CSS
 import AboutUs from './components/AboutUs';
 import CVgenerator from './components/CVgenerator';
 import Footer from './components/Footer';
 import HomeTab from './components/HomeTab';
 import Navbar from './components/Navbar';
 import TemplateTab from './components/TemplateTab';
+import 'nprogress/nprogress.css'; // Make sure this is imported for default styles
 
 function App() {
   let scrollTimeout;
 
   document.addEventListener('scroll', () => {
-    document.body.classList.add("showScrollbar");
+    document.body.classList.add('showScrollbar');
 
     clearTimeout(scrollTimeout);
 
     scrollTimeout = setTimeout(() => {
-      document.body.classList.remove("showScrollbar");
+      document.body.classList.remove('showScrollbar');
     }, 500);
   });
-
 
   return (
     <>
       <Router>
         <Navbar />
-        <MainContent/>
+        <MainContent />
       </Router>
-
-    {/* <TemplateTab/> */}
-      {/* <Navbar/>
-      <HomeTab/>
-      <Templates/>
-      <Footer/> */}
-      {/* <AboutUs/> */}
-      {/* <CVgenerator/> */}
-      {/* <CreativeTemplate/> */}
-      {/* <BasicTemplate/> */}
-      {/* <ProfessionalTemplate/> */}
-      {/* <ProfessionalIITemplate/> */}
     </>
   );
 }
+
 function MainContent() {
   const location = useLocation();
+
+  useEffect(() => {
+    NProgress.start(); // Start progress bar
+  
+    // Optionally, you might want to finish the progress after a certain event.
+    NProgress.done(); // Complete progress immediately
+  
+    return () => {
+      NProgress.done(); // Ensure progress bar is cleaned up on unmount
+    };
+  }, [location]);
+  
+
   const showFooter = location.pathname !== '/cvgenerator';
 
   return (
@@ -61,7 +64,7 @@ function MainContent() {
       </Routes>
       {showFooter && <Footer />}
     </>
-  )
+  );
 }
 
 export default App;
