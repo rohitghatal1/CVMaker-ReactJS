71c7feaa6e06f6029732a8213c80dc809cf81d70
rohitghatal1 <rohitghatal@gmail.com>
2024-09-29 15:37:33 +0545

    updated code for setting pressed in effectt

--- src/componentsInner/TemplatesDisplay.js
+++ src/componentsInner/TemplatesDisplay.js
@@ -1,31 +1,34 @@
-import React, {useState} from 'react'
-import './templatesDisplay.css'
+import React, { useState } from 'react';
+import './templatesDisplay.css';
 import templateData from '../templateSelector/templateData';
 import BasicTemplate from '../allTemplates/basicTemplate/BasicTemplate';
 
-export default function TemplatesDisplay({selectedTemplate, setSelectedTemplate, goToNextStep}) {
-  const [borderColor, setBorderColor] = useState({});
+export default function TemplatesDisplay({ selectedTemplate, setSelectedTemplate, goToNextStep }) {
+  const [selectedCardId, setSelectedCardId] = useState(null); // Track selected card ID
 
   const handleCardClick = (template) => {
     setSelectedTemplate(template.component);
-    setBorderColor({[template.id]: 'blue'})
+    setSelectedCardId(template.id); // Set the clicked template's ID as selected
   };
 
   return (
     <div>
       <div className="allTemplateDisplay">
-      <div className="templateContainerHeading">
-        <h2>Select a Template</h2>
-        <button className='nextbtn' onClick={goToNextStep}>Next <i className="fa-solid fa-arrow-right"></i></button>
-      </div>
+        <div className="templateContainerHeading">
+          <h2>Select a Template</h2>
+          <button className='nextbtn' onClick={goToNextStep}>Next <i className="fa-solid fa-arrow-right"></i></button>
+        </div>
         <div className="allTemplateInfo">
           <h3>Here are some of the available Template for CV</h3>
           <div className="allTemplateContainer">
             {templateData.map((template) => (
-              <div className="templateCard" key={template.id} 
-                onClick={()=> handleCardClick(template)} style={{borderColor: borderColor[template.id] || 'transparent'}}>
+              <div 
+                className={`templateCard ${selectedCardId === template.id ? 'selectedCard' : ''}`} 
+                key={template.id}
+                onClick={() => handleCardClick(template)}
+              >
                 <figure className="templateImage">
-                  <img src={template.image} alt="basicTemplate" />
+                  <img src={template.image} alt={template.name} />
                 </figure>
                 <label>{template.name}</label>
               </div>
@@ -34,5 +37,5 @@ export default function TemplatesDisplay({selectedTemplate, setSelectedTemplate,
         </div>
       </div>
     </div>
-  )
+  );
 }
