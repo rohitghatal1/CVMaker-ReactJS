0e38db32b8c0ae34dce8ae5bfc0ffe9bf8828a6f
rohitghatal1 <rohitghatal@gmail.com>
2024-10-01 16:33:37 +0545

    added developer photo

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './about.css'
+import developerPhoto from '../assets/personPhotos/profilePic.jpg'
 
 export default function AboutUs() {
   return (
@@ -7,16 +8,17 @@ export default function AboutUs() {
       <div className="aboutUsConponent">
         <h2>About Us</h2>
         <div className="aboutUsContainer">
-            <fieldset className="owner">
+          <fieldset className="owner">
             <legend>Developer</legend>
-                <figure><img src="" alt="" /></figure>
-                <div className="ownerInfo">
-                    <h3>Rohit Ghatal</h3>
-                </div>
-            </fieldset>
-            <div className="aboutApp">
-                
+            <div className="ownerInfo">
+              <h3>Rohit Ghatal</h3>
             </div>
+            <figure className='developerPhoto'><img src={developerPhoto} alt="" /></figure>
+          </fieldset>
+          
+          <div className="aboutApp">
+
+          </div>
         </div>
       </div>
     </div>
