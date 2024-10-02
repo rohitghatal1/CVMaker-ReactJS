fa2c1b483000730a2ee95a73fcf202671cac66a3
rohitghatal1 <rohitghatal@gmail.com>
2024-10-02 15:10:35 +0545

    added css for styling active nav link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import './navbar.css'
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../CVCraft.png'
 
-export default function Navbar({openForm}) {
+export default function Navbar({ openForm }) {
   return (
     <div>
       <nav>
         <div className="navbar">
-            <div className="logo"><img src={logo} alt="CVCraft" /></div>
-           
-            <div className="navitems">
-                <NavLink to = "/" className="navNavLinks" activeClassName ="active" exact>Home</NavLink>
-                <NavLink to = "/templateTab" className="navNavLinks" activeClassName ="active">Templates</NavLink>
-                <NavLink to = "/cvgenerator" className="navNavLinks" activeClassName ="active">CV-Generator</NavLink>
-                <NavLink to = "/about" className="navNavLinks" activeClassName ="active">About</NavLink>
-            </div>
-            <div className="loginSignup">
-                <button className='loginbtn' onClick={openForm}>Login / Register</button>
-            </div>
+          <div className="logo"><img src={logo} alt="CVCraft" /></div>
+
+          <div className="navitems">
+            <NavLink to="/" className="navlinks" activeClassName="active" exact>Home</NavLink>
+            <NavLink to="/templateTab" className="navlinks" activeClassName="active">Templates</NavLink>
+            <NavLink to="/cvgenerator" className="navlinks" activeClassName="active">CV-Generator</NavLink>
+            <NavLink to="/about" className="navlinks" activeClassName="active">About</NavLink>
+
+          </div>
+          <div className="loginSignup">
+            <button className='loginbtn' onClick={openForm}>Login / Register</button>
+          </div>
         </div>
       </nav>
     </div>
