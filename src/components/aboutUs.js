1e3884cd528717750c4aa1f2896eabdeca04cccb
rohitghatal1 <rohitghatal@gmail.com>
2024-10-03 15:30:10 +0545

    added send message form with styling

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -53,23 +53,27 @@ export default function AboutUs() {
           <div className="aboutApp">
             {/* Additional content for the app description can go here */}
           </div>
-          
+
           {/* send message section  */}
           <div className="sendMessageSection">
-            <div className="messageForm">
-
-            </div>
-            <button>
-              <div class="svg-wrapper-1">
-                <div class="svg-wrapper">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-                  </svg>
+            <form action="" className="sendMessageForm">
+              <input type="text" name='uName' placeholder='Your Name' />
+              <input type="text" name='uEmail' placeholder='Your Email'/>
+              <input type="text" name='uSubject' placeholder='Subject'/>
+              <input type="text" name='uMessage' placeholder='Message'/>
+              
+              <button className='sendMessageBtn' type='submit'>
+                <div class="svg-wrapper-1">
+                  <div class="svg-wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+                    </svg>
+                  </div>
                 </div>
-              </div>
-              <span>Send</span>
-            </button>
+                <span>Send</span>
+              </button>
+            </form>
           </div>
         </div>
       </div>
