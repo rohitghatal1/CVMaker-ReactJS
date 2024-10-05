7562045010836db79d3ff6053b375a3326f00d9e
rohitghatal1 <rohitghatal@gmail.com>
2024-10-05 13:33:38 +0545

    added hobbies section with new and old hobbies

--- src/componentsInner/Hobbies.js
+++ src/componentsInner/Hobbies.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default function Hobbies() {
-  return (
-    <div>
-      <div className="hobbiesComponents">
-        
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="hobbiesComponents">
+                <h2>Experience</h2>
+                <p>Provide your Experience details here</p>
+                <section className="hobbiesSection">
+                    <div className="currentHobbies">
+                        <p>Hobbies</p>
+                    </div>
+                    <div className="newHobbies">
+                        <p>New Hobby</p>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
 }
