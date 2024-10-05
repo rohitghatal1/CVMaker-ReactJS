781f49cb8f551d1348890b7ebb1da9e01978af85
rohitghatal1 <rohitghatal@gmail.com>
2024-10-05 13:50:14 +0545

    updated styles for hobby component

--- src/componentsInner/Hobbies.js
+++ src/componentsInner/Hobbies.js
@@ -8,15 +8,15 @@ export default function Hobbies({goToPreviousStep, goToNextStep}) {
     return (
         <div>
             <div className="hobbiesComponents">
-                <h2>Experience</h2>
-                <p>Provide your Experience details here</p>
+                <h2>Hobbies</h2>
+                <p>List out some of your Hobbies</p>
                 <section className="hobbiesSection">
                     <div className="currentHobbies">
                         <p>Hobbies</p>
                     </div>
                     <div className="newHobbies">
                         <p>New Hobby</p>
-                        <button className='addNewHobbybtn' onClick={openAddHobbyModal}><i className='fas fa-plus'></i> Add Experience</button>
+                        <button className='addNewHobbybtn' onClick={openAddHobbyModal}><i className='fas fa-plus'></i> Add Hobby</button>
                     </div>
                 </section>
             </div>
