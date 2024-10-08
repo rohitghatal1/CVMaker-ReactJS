73a658dfab85ee2c512d5189696ad0a1e95b2d25
rohitghatal1 <rohitghatal@gmail.com>
2024-10-08 17:33:38 +0545

    updated styles for hobbie item

--- src/componentsInner/Hobbies.js
+++ src/componentsInner/Hobbies.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './hobbies.css'
 
-export default function Hobbies({goToPreviousStep, goToNextStep}) {
+export default function Hobbies({ goToPreviousStep, goToNextStep }) {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isAddButtonVisible, setIsAddButtonVisible] = useState(true);
@@ -16,11 +16,11 @@ export default function Hobbies({goToPreviousStep, goToNextStep}) {
     }
 
     const [hobbies, setHobbies] = useState([]);
-    const [newHobbies, setNewHobbies] = useState({ hobbieName: ''})
+    const [newHobbies, setNewHobbies] = useState({ hobbieName: '' })
 
     const submitHobbyForm = (e) => {
         e.preventDefault();
-        if(newHobbies.hobbieName){
+        if (newHobbies.hobbieName) {
             const updatedHobbies = [...hobbies, newHobbies];
 
             setHobbies(updatedHobbies);
@@ -28,16 +28,16 @@ export default function Hobbies({goToPreviousStep, goToNextStep}) {
 
             setIsModalOpen(false);
             setIsAddButtonVisible(true);
-            setNewHobbies({hobbieName:''});
+            setNewHobbies({ hobbieName: '' });
         }
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         const storedHobbyData = JSON.parse(localStorage.getItem('hobbiesData'));
-        if(storedHobbyData){
+        if (storedHobbyData) {
             setHobbies(storedHobbyData)
         }
-    },[])
+    }, [])
 
     const deleteHobbie = (indexToDelete) => {
         const updatedHobbyData = hobbies.filter((_, index) => index !== indexToDelete);
@@ -54,15 +54,17 @@ export default function Hobbies({goToPreviousStep, goToNextStep}) {
                 <section className="hobbiesSection">
                     <div className="currentHobbies">
                         <p>Hobbies</p>
-                        {hobbies.length>0 ? (
-                            hobbies.map((hobby, index) =>(
-                                <div className="hobbieItem" key={index}>
-                                <p>{hobby.hobbieName}</p>
-                                <button className='deleteHobbyBtn' onClick={() => deleteHobbie(index)}><i className='fas fa-trash'></i></button>
-                                </div>
-                            ))
-                        )
-                        :(<p>No Hobbies Added yet!!! </p>)}
+                        <div className="hobbiesContainer">
+                            {hobbies.length > 0 ? (
+                                hobbies.map((hobby, index) => (
+                                    <div className="hobbieItem" key={index}>
+                                        <p>{hobby.hobbieName}</p>
+                                        <button className='deleteHobbyBtn' onClick={() => deleteHobbie(index)}><i className='fas fa-trash'></i></button>
+                                    </div>
+                                ))
+                            )
+                                : (<p>No Hobbies Added yet!!! </p>)}
+                        </div>
                     </div>
                     <div className="newHobbies">
                         <p>New Hobby</p>
@@ -70,7 +72,7 @@ export default function Hobbies({goToPreviousStep, goToNextStep}) {
                         {isModalOpen && <div className="hobbyModal">
                             <form onSubmit={submitHobbyForm}>
                                 <label htmlFor="title">Hobby Name:</label>
-                                <input type="text" name='hobbyName' placeholder='eg. Playing, Singing, Coding etc' value={newHobbies.hobbieName} onChange={(e)=> setNewHobbies({...newHobbies, hobbieName: e.target.value})}/>
+                                <input type="text" name='hobbyName' placeholder='eg. Playing, Singing, Coding etc' value={newHobbies.hobbieName} onChange={(e) => setNewHobbies({ ...newHobbies, hobbieName: e.target.value })} />
 
                                 <div className="submitAndCloseBtns">
                                     <button type='submit' className='submitBtn'><i class="fa-regular fa-paper-plane"></i> Submit</button>
