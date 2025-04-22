62b1766b16d92b3499d612b2611c8da1f13c3a8d
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-04-22 22:21:11 +0545

    updated

--- src/componentsInner/Awards.js
+++ src/componentsInner/Awards.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './awards.css'
 import { faL } from '@fortawesome/free-solid-svg-icons';
+import { Form, Input } from 'antd';
+import { useForm } from 'antd/es/form/Form';
 
-export default function Awards({goToPreviousStep, goToNextStep}) {
+export default function Awards({ goToPreviousStep, goToNextStep }) {
+  const [awardForm] = useForm();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isAddButtonVisible, setIsAddButtonVisible] = useState(true);
 
@@ -97,18 +100,19 @@ export default function Awards({goToPreviousStep, goToNextStep}) {
           <div className="newAwards">
             <p>New Achievement</p>
             {isModalOpen && <div className="addNewAwardModal">
-              <form onSubmit={submitAwardForm}>
+              <Form onFinish={submitAwardForm} form={awardForm}>
+
                 <label htmlFor="awardTitle">Title</label>
-                <input type="text" placeholder='eg: Best employee award' name='awardTitle' value={newAwards.awardTitle} onChange={handleInputChange} />
+                <Input type="text" placeholder='eg: Best employee award' name='awardTitle' value={newAwards.awardTitle} onChange={handleInputChange} />
 
                 <label htmlFor="organization">Organization</label>
-                <input type="text" placeholder='organization' name='organization' value={newAwards.organization} onChange={handleInputChange} />
+                <Input type="text" placeholder='organization' name='organization' value={newAwards.organization} onChange={handleInputChange} />
 
                 <label htmlFor="location">Location</label>
-                <input type="text" placeholder='location' name='location' value={newAwards.location} onChange={handleInputChange} />
+                <Input type="text" placeholder='location' name='location' value={newAwards.location} onChange={handleInputChange} />
 
                 <label htmlFor="receivedYear">Received Year</label>
-                <input type="text" placeholder='receivedYear' name='receivedYear' value={newAwards.receivedYear} onChange={handleInputChange} />
+                <Input type="text" placeholder='receivedYear' name='receivedYear' value={newAwards.receivedYear} onChange={handleInputChange} />
 
                 <label htmlFor="desc">Description</label>
                 <textarea rows={5} placeholder="Describe about the award" name='description' value={newAwards.description} onChange={handleInputChange}></textarea>
@@ -118,11 +122,12 @@ export default function Awards({goToPreviousStep, goToNextStep}) {
                   <button className='closeBtn' onClick={handleCloseForm}>Close</button>
                 </div>
 
-              </form>
+              </Form>
             </div>}
 
             {isAddButtonVisible && <button className='addNewAwardbtn' onClick={openAddAwardModal}><i className='fas fa-plus'></i> Add Award</button>}
           </div>
+
           <div className="nextAndBackBtn">
             <button className='prevBtn' onClick={goToPreviousStep}><i class="fa-solid fa-arrow-left"></i> Previous</button>
             <button className='nextBtn' onClick={goToNextStep}>Next <i class="fa-solid fa-arrow-right"></i></button>
